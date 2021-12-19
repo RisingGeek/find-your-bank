2eab1fc024632d906230ef5845cad671b97888bd
RisingGeek <bhavay.puri@partsavatar.ca>
2021-12-20 01:41:27 +0530

    Add homepage featuress :tada:

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,32 @@ import React from 'react';
 const HomePage = () => (
   <div>
     <h1>Find Your Bank</h1>
+    <h3 className="mt-4">Salient Features</h3>
+    <ul className="list-group mb-4">
+      <li className="list-group-item">
+        List all banks based on City with filter/search logic
+      </li>
+      <li className="list-group-item">
+        Individual bank details based on IFSC
+      </li>
+      <li className="list-group-item">
+        Pagination (editable)
+      </li>
+      <li className="list-group-item">
+        API response caching
+      </li>
+      <li className="list-group-item">
+        Edge cases handled
+      </li>
+      <li className="list-group-item">
+        Mark bank as favorite
+      </li>
+    </ul>
+
+    <p>
+      For pagination, I have added manual pagination instead of using a library like
+      react-paginate
+    </p>
   </div>
 );
 
